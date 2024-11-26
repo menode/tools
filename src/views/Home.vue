3046096099bdc7c4ded3f2d05df4a50434e8a0a2
ewven <ewven@ewvendeMac-mini.local>
2024-11-26 21:40:57 +0800

    dev

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="home">
+        <h1>Welcome to Home Page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+
+<style scoped>
+.home {
+    text-align: center;
+    margin-top: 50px;
+}
+</style>
